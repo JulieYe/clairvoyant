2d4bc4c3addd96d751d9e11ed997a2fd0bc5bdb4
Fei Ye <fei.ye@avantcredit.com>
2015-01-05 17:09:22 -0600

    add tup_tests and count_cpu_cycles

---
+++ playground/count_cpu_cycles.cpp
@@ -0,0 +1,15 @@
+#include <stdint.h>
+#include <iostream>
+
+uint64_t rdtsc() {
+  unsigned int lo,hi;
+  __asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
+  return ((uint64_t)hi << 32) | lo;
+}
+
+int main() {
+  std::cout << rdtsc() << std::endl;
+  std::cout << rdtsc() << std::endl;
+  std::cout << rdtsc() << std::endl;
+  return 0;
+}
